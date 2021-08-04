641088b3fc6817d74fa5eb72a0698a673e0809ce
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-08-04 08:56:45 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,11 @@
+
+/*** Mini-Challenge  */
+// Satzbau + Punktuation
+
+// Vereinbarung:
+// "S" (sentence) --> "."  Ich bin Max.
+// "Q" (question) --> "?"  Bist du Max?
+// "E" (exclamation) --> "!"  Ich bin!
+
+// Parameter --> funktion satzbau([ ...], "Q"){ ...}
+
