5477c7d6baa2f4ef2e29f976e5b8005a65449cd0
lilu1982 <homenetmedia82@gmail.com>
2021-08-04 11:35:04 +0200

    bearbeitet

--- script_01.js
+++ script_01.js
@@ -1,4 +1,3 @@
-
 /*** Mini-Challenge  */
 // Satzbau + Punktuation
 
@@ -9,3 +8,52 @@
 
 // Parameter --> funktion satzbau([ ...], "Q"){ ...}
 
+
+/*** Funktion mit Array als Parameter */
+// mögliche Tests:
+ausgabe(getSentence(["Ich","bin","Peter"],"S"));
+ausgabe(getSentence(["Bist","du","Peter"],"Q"));
+ausgabe(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+ausgabe(getSentence(["Ich","bin"],"E"));
+ausgabe(getSentence(["Ich","bin"],"#"));
+function getSentence(arr,flag) {
+    
+    const gap     = " ";
+    const punct   = getPunct(flag); //.
+    let str     = "";
+
+    for (let i = 0; i < arr.length; i++) 
+    {
+        if (i != (arr.length - 1)) { 
+            str +=  arr[i] + gap;
+        } else {
+            str +=  arr[i] + punct;
+        }
+    }                
+
+    return str ; 
+}
+
+/*** Service-Funktion / Punktuation */
+// ausgabe(getPunct("S"));
+// ausgabe(getPunct("Q"));
+// ausgabe(getPunct("E"));
+// ausgabe(getPunct("#"));
+function getPunct(checkStr) {
+    switch (checkStr) {
+        case "S":
+            return ".";
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return "."; // TBA
+    }
+}
+
+/*** Ausgabe */
+// ausgabe("hi");
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
